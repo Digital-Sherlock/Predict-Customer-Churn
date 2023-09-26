80a27edc3708fc8352de3ad4c619b39cc3b33e82
Digital-Sherlock <vadimka10497@gmail.com>
2023-09-25 20:45:53 -0700

    Work started on testing and modules

---
+++ churn_library.py
@@ -0,0 +1,139 @@
+'''
+This module converts churn_notebook.ipynb into a modular code that
+is easy to test and debug. It includes functions for:
+1) importing the data
+2) performing EDA
+3) encoding cat data
+4) performing feature engineering
+5) training a model
+6) generating classification report
+7) plotting feature importance
+
+Date: 2023-09-25
+Author: Vadim Polovnikov
+'''
+
+# import libraries
+import os
+os.environ['QT_QPA_PLATFORM']='offscreen'
+
+import shap # building and explaining graphs
+import joblib # saving scikit-learn models
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+
+from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import plot_roc_curve, classification_report
+
+
+def import_data(pth):
+    '''
+    Returns Pandas DataFrame.
+    Input:
+        - pth: (str) file path
+    Output:
+        - Pandas DF
+    '''
+    df = pd.read_csv(pth)
+    return df
+
+
+
+
+def perform_eda(df):
+    '''
+    perform eda on df and save figures to images folder
+    input:
+            df: pandas dataframe
+
+    output:
+            None
+    '''
+    plt.figure(figsize=(10,5))
+
+
+def encoder_helper(df, category_lst, response):
+    '''
+    helper function to turn each categorical column into a new column with
+    propotion of churn for each category - associated with cell 15 from the notebook
+
+    input:
+            df: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used for naming variables or index y column]
+
+    output:
+            df: pandas dataframe with new columns for
+    '''
+    pass
+
+
+def perform_feature_engineering(df, response):
+    '''
+    input:
+              df: pandas dataframe
+              response: string of response name [optional argument that could be used for naming variables or index y column]
+
+    output:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    '''
+
+def classification_report_image(y_train,
+                                y_test,
+                                y_train_preds_lr,
+                                y_train_preds_rf,
+                                y_test_preds_lr,
+                                y_test_preds_rf):
+    '''
+    produces classification report for training and testing results and stores report as image
+    in images folder
+    input:
+            y_train: training response values
+            y_test:  test response values
+            y_train_preds_lr: training predictions from logistic regression
+            y_train_preds_rf: training predictions from random forest
+            y_test_preds_lr: test predictions from logistic regression
+            y_test_preds_rf: test predictions from random forest
+
+    output:
+             None
+    '''
+    pass
+
+
+def feature_importance_plot(model, X_data, output_pth):
+    '''
+    creates and stores the feature importances in pth
+    input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+
+    output:
+             None
+    '''
+    pass
+
+def train_models(X_train, X_test, y_train, y_test):
+    '''
+    train, store model results: images + scores, and store models
+    input:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    output:
+              None
+    '''
+    pass
