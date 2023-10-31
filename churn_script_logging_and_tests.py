06c51c4b853df3f6e284259e3d418abb65d25453
Digital-Sherlock <vadimka10497@gmail.com>
2023-10-30 19:19:11 -0700

    Main scrips and their tests finished.

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -10,125 +10,141 @@ import pandas as pd
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 def test_import(import_data):
-	'''
-	test data_import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data(): SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda(): The file wasn't found")
-		raise err
+    '''
+    test data_import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        df = import_data("./data/bank_data.csv")
+        logging.info("Testing import_data(): SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda(): The file wasn't found")
+        raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data(): The file doesn't appear to have rows and columns")
-		raise err
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data(): The file doesn't appear to have rows and columns")
+        raise err
 
 
 def test_eda(perform_eda, df, col):
-	'''
-	test perform eda function
-	'''
-	try:
-		assert isinstance(df, pd.DataFrame)
-		assert isinstance(col, str)
-	except AssertionError:
-		logging.error('Testing perform_eda(): incorrect data type! df has to be pandas DF and col - string.')
+    '''
+    test perform eda function
+    '''
+    try:
+        assert isinstance(df, pd.DataFrame)
+        assert isinstance(col, str)
+    except AssertionError:
+        logging.error(
+            'Testing perform_eda(): incorrect data type! df has to be pandas DF and col - string.')
 
-	try:
-		perform_eda(df, col)
-		logging.info(f'Testing perform_eda(): Histogram has been plotted ({col})')
-	except KeyError:
-		logging.error(f'Testing perform_eda(): The {col} column is missing. Failed to plot a graph.')
-		
+    try:
+        perform_eda(df, col)
+        logging.info(
+            f'Testing perform_eda(): Histogram has been plotted ({col})')
+    except KeyError:
+        logging.error(
+            f'Testing perform_eda(): The {col} column is missing. Failed to plot a graph.')
 
 
 def test_encoder_helper(encoder_helper, category, new_cat_name, df):
-	'''
-	test encoder helper
-	'''
-	try:
-		assert isinstance(category, str)
-		assert isinstance(new_cat_name, str)
-		assert isinstance(df, pd.DataFrame)
-	except AssertionError as err:
-		logging.error('Testing test_encoder_helper(): incorrect data type!')
-		raise err
+    '''
+    test encoder helper
+    '''
+    try:
+        assert isinstance(category, str)
+        assert isinstance(new_cat_name, str)
+        assert isinstance(df, pd.DataFrame)
+    except AssertionError as err:
+        logging.error('Testing test_encoder_helper(): incorrect data type!')
+        raise err
 
-	try:
-		encoder_helper(df, category, new_cat_name)
-		logging.info('Testing test_encoder_helper(): features succesfully transformed.')
-	except KeyError as err:
-		logging.error(f'Testing test_encoder_helper(): no such column in df ({category})!')
-		raise err
-	except ValueError as err:
-		logging.error('Testing test_encoder_helper(): Values missing in encoded column.')
+    try:
+        encoder_helper(df, category, new_cat_name)
+        logging.info(
+            'Testing test_encoder_helper(): features succesfully transformed.')
+    except KeyError as err:
+        logging.error(
+            f'Testing test_encoder_helper(): no such column in df ({category})!')
+        raise err
+    except ValueError as err:
+        logging.error(
+            'Testing test_encoder_helper(): Values missing in encoded column.')
 
 
 def test_perform_feature_engineering(perform_feature_engineering, df):
-	'''
-	test perform_feature_engineering
-	'''
-	try:
-		assert isinstance(df, pd.DataFrame)
-	except AssertionError as err:
-		logging.error('Testing test_perform_feature_engineering(): Wrong data type! Only accepts pd.DataFrame.')
-		raise err
-	
-	try:
-		perform_feature_engineering(df)
-		logging.info('Testing test_perform_feature_engineering(): Feature engineering succesfully performed.')
-	except KeyError as err:
-		logging.error('Testing test_perform_feature_engineering(): Feature doesn\'t exists in the original dataset')
-		raise err
+    '''
+    test perform_feature_engineering
+    '''
+    try:
+        assert isinstance(df, pd.DataFrame)
+    except AssertionError as err:
+        logging.error(
+            '''Testing test_perform_feature_engineering(): Wrong data type!
+            Only accepts pd.DataFrame.''')
+        raise err
+
+    try:
+        perform_feature_engineering(df)
+        logging.info(
+            '''Testing test_perform_feature_engineering():
+            Feature engineering succesfully performed.''')
+    except KeyError as err:
+        logging.error(
+            '''Testing test_perform_feature_engineering(): Feature doesn\'t exists
+            in the original dataset''')
+        raise err
 
 
-def test_train_models(train_models, X_train, X_test, y_train, y_test):
-	'''
-	test train_models
-	'''
-	try:
-		assert isinstance(X_train, pd.DataFrame)
-		assert isinstance(X_test, pd.DataFrame)
-		assert isinstance(y_train, pd.Series)
-		assert isinstance(y_test, pd.Series)
-	except AssertionError as err:
-		logging.error('Testing test_train_models(): Incorrect data type! X_ have to be pd.DataFrame, y_ - pd.Series.')
-		raise err
-	try:
-		train_models(X_train, X_test, y_train, y_test)
-		logging.info('Testing train_models(): models have been succesfully trained.')
-	except FileNotFoundError:
-		logging.error('Testing train_models(): models failed to be saved! Make sure the storage path exists.')
+def test_train_models(train_models, x_train, x_test, y_train, y_test):
+    '''
+    test train_models
+    '''
+    try:
+        assert isinstance(x_train, pd.DataFrame)
+        assert isinstance(x_test, pd.DataFrame)
+        assert isinstance(y_train, pd.Series)
+        assert isinstance(y_test, pd.Series)
+    except AssertionError as err:
+        logging.error(
+            '''Testing test_train_models(): Incorrect data type!
+            X_ have to be pd.DataFrame, y_ - pd.Series.''')
+        raise err
+    try:
+        train_models(x_train, x_test, y_train, y_test)
+        logging.info(
+            'Testing train_models(): models have been succesfully trained.')
+    except FileNotFoundError:
+        logging.error(
+            'Testing train_models(): models failed to be saved! Make sure the storage path exists.')
 
 
 if __name__ == "__main__":
-	# testing import_data()
-	import churn_library as cl
-	test_import_df = test_import(cl.import_data)
+    # testing import_data()
+    import churn_library as cl
+    test_import(cl.import_data)
 
-	# testing perform_eda()
-	test_perform_eda_churn_col = test_eda(cl.perform_eda, cl.df, col='Churn')
-	test_perform_eda_churn_cx_age_col = test_eda(cl.perform_eda, cl.df, col='Customer_Age')
-	test_perform_eda_churn_marital_status_col = test_eda(cl.perform_eda, cl.df, col='Marital_Status')
-	test_perform_eda_otal_trans_ct_col = test_eda(cl.perform_eda, cl.df, col='Total_Trans_Ct')
+    # testing perform_eda()
+    test_eda(cl.perform_eda, cl.df, col='Churn')
+    test_eda(cl.perform_eda, cl.df, col='Customer_Age')
+    test_eda(cl.perform_eda, cl.df, col='Marital_Status')
+    test_eda(cl.perform_eda, cl.df, col='Total_Trans_Ct')
 
-	# testing encoder_helper()
-	for cat in cl.cat_columns:
-		test_encoder_helper(cl.encoder_helper, cat, f'{cat}_Churn', cl.df)
+    # testing encoder_helper()
+    for cat in cl.cat_columns:
+        test_encoder_helper(cl.encoder_helper, cat, f'{cat}_Churn', cl.df)
 
-	# testing perform_feature_engineering():
-	test_perform_feature_engineering(cl.perform_feature_engineering, cl.df)
+    # testing perform_feature_engineering():
+    test_perform_feature_engineering(cl.perform_feature_engineering, cl.df)
 
-	# testing train_models()
-	test_train_models(cl.train_models, cl.x_train, cl.x_test, 
-				   cl.y_train, cl.y_test)
+    # testing train_models()
+    test_train_models(cl.train_models, cl.x_train, cl.x_test,
+                      cl.y_train, cl.y_test)
--- churn_script_logging_and_tests_refactored.py
+++ churn_script_logging_and_tests_refactored.py
@@ -5,41 +5,41 @@ churn_library_refactored.py.
 Author: Vadim Polovnikov
 Date: 2023-10-06
 '''
-
-import os
 import logging
 import pandas as pd
 import churn_library_refactored as clr
-
-from constants import PATH
-from constants import CAT_COLUMNS
-from constants import KEEP_COLS
-from constants import IMAGES_PATH_EDA
+from constants import path
+from constants import cat_columns
+from constants import keep_cols
+from constants import images_path_eda
+from churn_library_refactored import x_train, x_test, y_train, y_test
 
 
 logging.basicConfig(
     filename='./logs/churn_library_refactored.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
-def test_import_data(import_data, path=PATH):
+def test_import_data(import_data, input_path=path):
     '''
     Testing import_data.
 
     Input:
         - import_data: (func) import_data function
+        - output_path: (str) dataset file path
     Output:
         - None
     '''
     try:
-        assert isinstance(path, str)
+        assert isinstance(input_path, str)
     except AssertionError as err:
-       logging.error('''Testing import_data(): wrong data type for data import path.
+        logging.error(
+            '''Testing import_data(): wrong data type for data import path.
                      Exepcting string.''')
-       raise err
-    
+        raise err
+
     try:
         import_data(path)
         logging.info('Testing import_data(): successful data import.')
@@ -48,7 +48,7 @@ def test_import_data(import_data, path=PATH):
         raise err
 
 
-def test_encode_helper(encode_helper, df, cat_columns=CAT_COLUMNS):
+def test_encode_helper(encode_helper, df, cat_columns=cat_columns):
     '''
     Testing encode_helper.
 
@@ -65,10 +65,11 @@ def test_encode_helper(encode_helper, df, cat_columns=CAT_COLUMNS):
     except AssertionError as err:
         logging.error('''Testing test_encode_helper(): wrong data type!
                       Make sure pd.DataFrame and list of cat features are supplied.''')
-        
+
     try:
         encode_helper(df, cat_columns)
-        logging.info('Testing test_encode_helper(): categories successfully encoded.')
+        logging.info(
+            'Testing test_encode_helper(): categories successfully encoded.')
     except KeyError as err:
         logging.error('''Testing test_encode_helper(): onehot_encode() failed!
                       Supplied list of cat columns doesn\'t match input dataset columns.''')
@@ -92,23 +93,28 @@ def test_perform_feature_engineering(perform_feature_engineering, df):
         raise err
 
     try:
-        X_train, X_test, y_train, y_test = perform_feature_engineering(df)
+        x_train_arr, x_test_arr, y_train_arr, y_test_arr = perform_feature_engineering(
+            df)
         # checking num of cols in dataset
-        assert len(X_train.columns) and len(X_test.columns) == len(KEEP_COLS)
-        logging.info('''Testing perform_feature_engineering(): feature engineering
+        assert len(x_train_arr.columns) and len(
+            x_test_arr.columns) == len(keep_cols)
+        logging.info(
+            '''Testing perform_feature_engineering(): feature engineering
                      successfully performed.''')
     except KeyError as err:
-        logging.error('''Testing perform_feature_engineering(): data_splitter() failed! Make sure
+        logging.error(
+            '''Testing perform_feature_engineering(): data_splitter() failed! Make sure
                       "Churn" column is present in the input dataset.''')
         raise err
     except AssertionError as err:
-        logging.error('''Testing perform_feature_engineering(): wrong number of columns!
+        logging.error(
+            '''Testing perform_feature_engineering(): wrong number of columns!
                       Output dataset columns should be equal to KEEP_COLS.''')
         raise err
 
 
 def test_perform_eda(perform_eda, col, type, filename,
-                     PATH=IMAGES_PATH_EDA, **kwargs):
+                     PATH=images_path_eda, **kwargs):
     '''
     Tests perform_eda.
     Input:
@@ -119,12 +125,19 @@ def test_perform_eda(perform_eda, col, type, filename,
     '''
     try:
         perform_eda(col, type, filename, PATH, **kwargs)
-        logging.info('Testing perform_eda(): Plots have been successfully saved.')
+        logging.info(
+            'Testing perform_eda(): Plots have been successfully saved.')
     except KeyError:
-        logging.error('Testing perform_eda(): Supplied column is missing in the dataset.')
-        
+        logging.error(
+            'Testing perform_eda(): Supplied column is missing in the dataset.')
 
-def test_train_models(train_models, X_train, X_test, y_train, y_test):
+
+def test_train_models(
+        train_models,
+        x_train_arr,
+        x_test_arr,
+        y_train_arr,
+        y_test_arr):
     '''
     Tests train_models.
     Input:
@@ -134,24 +147,26 @@ def test_train_models(train_models, X_train, X_test, y_train, y_test):
         - None
     '''
     try:
-        assert isinstance(X_train, pd.DataFrame)
-        assert isinstance(X_test, pd.DataFrame)
-        assert isinstance(y_train, pd.Series)
-        assert isinstance(y_test, pd.Series)
+        assert isinstance(x_train_arr, pd.DataFrame)
+        assert isinstance(x_test_arr, pd.DataFrame)
+        assert isinstance(y_train_arr, pd.Series)
+        assert isinstance(y_test_arr, pd.Series)
     except AssertionError as err:
         logging.error('''Testing test_train_models(): Incorrect data type!
                       X_ have to be pd.DataFrame, y_ - pd.Series.''')
         raise err
     try:
-        train_models(X_train, X_test, y_train, y_test)
-        logging.info('Testing train_models(): models have been succesfully trained.')
+        train_models(x_train_arr, x_test_arr, y_train_arr, y_test_arr)
+        logging.info(
+            'Testing train_models(): models have been succesfully trained.')
     except FileNotFoundError:
-	    logging.error('Testing train_models(): models failed to be saved! Make sure the storage path exists.')
+        logging.error(
+            'Testing train_models(): models failed to be saved! Make sure the storage path exists.')
 
 
 if __name__ == "__main__":
     # testing import_data
-    test_import_data(clr.import_data, PATH)
+    test_import_data(clr.import_data, path)
 
     # testing encode_helper
     test_encode_helper(clr.encode_helper, clr.df)
@@ -161,34 +176,33 @@ if __name__ == "__main__":
 
     # testing perform_eda
     test_perform_eda(
-                    clr.perform_eda,
-                    col='Churn',
-                    type='matplotlib',
-                    filename='churn_hist.png',
-                    kind='hist',
-                    xlabel='Churn: 1 - yes, 0  - no',
-                    ylabel='Number of customers')
+        clr.perform_eda,
+        col='Churn',
+        type='matplotlib',
+        filename='churn_hist.png',
+        kind='hist',
+        xlabel='Churn: 1 - yes, 0  - no',
+        ylabel='Number of customers')
     test_perform_eda(
-                    clr.perform_eda,
-                    col='Customer_Age',
-                    type='matplotlib',
-                    filename='churn_cx_age.png',
-                    kind='hist',
-                    xlabel='Customers\' age',
-                    ylabel='Number of Customers')
+        clr.perform_eda,
+        col='Customer_Age',
+        type='matplotlib',
+        filename='churn_cx_age.png',
+        kind='hist',
+        xlabel='Customers\' age',
+        ylabel='Number of Customers')
     test_perform_eda(
-                    clr.perform_eda,
-                    col='Total_Trans_Ct',
-                    type='sns',
-                    kind='histplot',
-                    filename='total_trans_ct.png',)
+        clr.perform_eda,
+        col='Total_Trans_Ct',
+        type='sns',
+        kind='histplot',
+        filename='total_trans_ct.png',)
     test_perform_eda(
-                    clr.perform_eda,
-                    col='N/A',
-                    type='sns',
-                    kind='corr',
-                    filename='corr_matrix.png',)
-    
+        clr.perform_eda,
+        col='N/A',
+        type='sns',
+        kind='corr',
+        filename='corr_matrix.png',)
+
     # testing train_models
-    from churn_library_refactored import X_train, X_test, y_train, y_test
-    test_train_models(clr.train_models, X_train, X_test, y_train, y_test)
+    test_train_models(clr.train_models, x_train, x_test, y_train, y_test)
